840e59cf7ce8343c22a35a77272de741793d64b6
augustinde <augustin.1201@outlook.fr>
2023-12-12 15:29:52 +0100

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,28 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Augustin Desaintfucien on 12/12/2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct TodolistApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todolist/TodolistData.swift
@@ -0,0 +1,14 @@
+//
+//  TodolistData.swift
+//  Todolist
+//
+//  Created by Augustin Desaintfucien on 12/12/2023.
+//
+
+import Foundation
+
+struct TodolistData{
+    static var todolist: [String] = []
+}
+
+
